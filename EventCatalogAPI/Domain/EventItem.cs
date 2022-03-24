c7c4ba2a2cd36d7ff2005bc9ea2fd7c9c5870815
Michelle Burroughs <burroughs87@gmail.com>
2022-03-23 18:25:01 -0700

    Added EventItem Domain and Helper file

---
+++ EventCatalogAPI/Domain/EventItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Domain
+{
+
+    public class EventItem
+    {
+        public int Id { get; set; }
+        public string EventName { get; set; }
+        public string EventDescription { get; set; }
+        public DateTime EventDate { get; set; }
+        public DateTime EventTime { get; set; }
+        public string MainEventImageUrl { get; set; }
+        public List<string> AdditionalEventImagesUrls { get; set; }
+        public decimal Price { get; set; }
+        public ImmutableList<string> RefundPolicy { get; set; }
+        public List<string> EventTags { get; set; }
+
+        public int EventTypeId { get; set; }
+        public virtual EventType EventType { get; set; }
+
+        public int EventCatagoryId { get; set; }
+        public virtual EventCatagory EventCatagory { get; set; }
+    }
+}
---
+++ EventCatalogAPI/Domain/Helper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Domain
+{
+    public class Helper
+    {
+        public List<string> RefundPolicyBeforeImmutable { get; set; }
+
+        public ImmutableList<string> RefundPolicies { get; set; }
+
+        public Helper() {
+
+            RefundPolicyBeforeImmutable = new List<string>();
+            RefundPolicyBeforeImmutable.Add("Up to 1 day before the event starts");
+            RefundPolicyBeforeImmutable.Add("Up to 7 days before the event starts");
+            RefundPolicyBeforeImmutable.Add("Up to 30 days before the event starts");
+            RefundPolicyBeforeImmutable.Add("On a case-by-case basis");
+            RefundPolicyBeforeImmutable.Add("No refunds");
+
+            RefundPolicies = RefundPolicyBeforeImmutable.ToImmutableList();
+        }
+    }
+}
