ba0327060a77d4224c86532d695ec16ffd0b4c6f
Michelle Burroughs <burroughs87@gmail.com>
2022-03-24 16:13:58 -0700

    Modified EventContext.cs and StartUp file for dependency Injection Started Seed File

--- EventCatalogAPI/Domain/EventCategory.cs
+++ EventCatalogAPI/Domain/EventCategory.cs
@@ -8,6 +8,6 @@ namespace EventCatalogAPI.Domain
     public class EventCategory
     {
         public int Id { get; set; }
-        public int Category { get; set; }
+        public string Category { get; set; }
     }
 }
