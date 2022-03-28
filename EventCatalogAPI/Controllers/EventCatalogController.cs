d3662ec604f4a344c8e72db8a3517b64c2810700
gessyz123 <yasmine.a.elezaby@gmail.com>
2022-03-28 13:30:05 -0700

    added more APIs

--- EventCatalogAPI/Controllers/EventCatalogController.cs
+++ EventCatalogAPI/Controllers/EventCatalogController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,22 +18,50 @@ namespace EventCatalogAPI.Controllers
         public EventCatalogController(EventContext context)
         {
             _context = context;
-            
+
         }
-        [HttpGet("action")]
-        public async Task<IActionResult> EventCategories() // the name of the thread
+        [HttpGet("[action]")]
+        public async Task<IActionResult> EventCategories() //The name of the thread
         {
-            //This task ( thread)will query the data base and get all the event categories and converet them to a list 
-            var categories =await _context.EventCategories.ToListAsync();
+            //This task (thread) will query the data base and get all the event categories and convert them to a list 
+            var categories = await _context.EventCategories.ToListAsync();
             return Ok(categories);
 
         }
 
-        [HttpGet("action")]
+        [HttpGet("[action]")]
         public async Task<IActionResult> EventTypes()
         {
             var types = await _context.EventTypes.ToListAsync();
             return Ok(types);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> EventMetroCity()
+        {
+            var cities = await _context.EventMetroCities.ToListAsync();
+            return Ok(cities);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> EventOrganizer()
+        {
+            var organizers = await _context.EventOrganizers.ToListAsync();
+            return Ok(organizers);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> EventItem(
+            [FromQuery] int pageIndex = 0,
+            [FromQuery] int pageSize = 6)
+        {
+            var items = await _context.Events
+                 .OrderBy(e => e.EventMetroCity)
+                 .Skip(pageIndex * pageSize)
+                 .Take(pageSize)
+                 .ToListAsync();
+
+            return Ok(items);
         }
 
     }
