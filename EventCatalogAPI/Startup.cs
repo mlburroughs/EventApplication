f538b2d727301d5cd20bab07068f0f9187de292f
Michelle Burroughs <burroughs87@gmail.com>
2022-04-02 15:11:05 -0700

    fixed docker file and updated startup file for configuration

--- EventCatalogAPI/Startup.cs
+++ EventCatalogAPI/Startup.cs
@@ -28,8 +28,25 @@ namespace EventCatalogAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddDbContext<EventContext>(options => 
-                options.UseSqlServer(Configuration["ConnectionString"]));// using SQL service
+
+            var dbServer = Configuration["DatabaseServer"];
+            var dbName = Configuration["DatabaseName"];
+            var dbUser = Configuration["DatabaseUser"];
+            var dbPassword = Configuration["DatabasePassword"];
+            var connectionString = $"Data Source={dbServer};Initial Catalog={dbName};User Id={dbUser};Password={dbPassword};Connect Timeout=30;";
+
+            services.AddDbContext<EventContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "EventsOnContainers - Event Catalog API",
+                    Version = "v1",
+                    Description = "Event catalog Microservice"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +62,12 @@ namespace EventCatalogAPI
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger()
+                .UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "EventCatalogAPI V1");
+                });
 
             app.UseEndpoints(endpoints =>
             {
