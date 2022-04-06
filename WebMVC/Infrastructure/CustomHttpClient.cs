706a0cd1c6ab51abae52287e0df17d8ecdd47cf0
gessyz123 <yasmine.a.elezaby@gmail.com>
2022-04-06 14:01:48 -0700

    web mvc part 1

---
+++ WebMVC/Infrastructure/CustomHttpClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebMVC.Infrastructure
+{
+    public class CustomHttpClient : IHttpClient
+    {
+        private readonly HttpClient _client;
+
+        public CustomHttpClient()
+        {
+            _client = new HttpClient();
+        }
+        public Task<HttpResponseMessage> DeleteAsync<T>(string uri, string authorizationtodekn = null, string authorizationmethod = "Bearer")
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> GetStringAsync(string uri, string authorizationtodekn = null, string authorizationmethod = "Bearer")
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get,uri);
+            var response= await _client.SendAsync(requestMessage);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public Task<HttpResponseMessage> PostAsync<T>(string uri, T item, string authorizationtodekn = null, string authorizationmethod = "Bearer")
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> PutAsync<T>(string uri, T item, string authorizationtodekn = null, string authorizationmethod = "Bearer")
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebMVC/Infrastructure/IHttpClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace WebMVC.Infrastructure
+{
+    public interface IHttpClient
+    {
+        Task<string> GetStringAsync(string uri, string authorizationtodekn=null,
+            string authorizationmethod="Bearer");
+
+        Task<HttpResponseMessage> PostAsync <T>(string uri, T item,
+            string authorizationtodekn = null,
+            string authorizationmethod = "Bearer");
+
+        Task<HttpResponseMessage> PutAsync<T>(string uri, T item,
+            string authorizationtodekn = null,
+            string authorizationmethod = "Bearer");
+
+        Task<HttpResponseMessage> DeleteAsync<T>(string uri,
+            string authorizationtodekn = null,
+            string authorizationmethod = "Bearer");
+    }
+}
