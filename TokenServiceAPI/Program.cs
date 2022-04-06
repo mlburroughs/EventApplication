8ab800078b14e72e0af770f08e51f1716336c84d
Michelle Burroughs <burroughs87@gmail.com>
2022-04-06 11:44:47 -0700

    Added Migrations

--- TokenServiceAPI/Program.cs
+++ TokenServiceAPI/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TokenServiceAPI.Data;
 
 namespace TokenServiceAPI
 {
@@ -13,7 +16,16 @@ namespace TokenServiceAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProviders = scope.ServiceProvider;
+                var context = serviceProviders.GetRequiredService<ApplicationDbContext>();
+                var userManager = serviceProviders.GetRequiredService<UserManager<IdentityUser>>();
+                IdentityDbInit.Initialize(context, userManager).Wait();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
