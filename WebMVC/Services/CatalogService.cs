ae84e1c4be2a466118cf3249ae90231451b98a7c
gessyz123 <yasmine.a.elezaby@gmail.com>
2022-04-06 14:11:45 -0700

    catalog service

---
+++ WebMVC/Services/CatalogService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVC.Services
+{
+    public class CatalogService
+    {
+        public async Task<IEnumerable<SelectListItem>> GetEventTypesAsync()
+        {
+            
+        }
+    }
+}
