8ab8b646e8ead7ca8c7b8d302d4ac04735e6b943
Michelle Burroughs <burroughs87@gmail.com>
2022-04-07 16:40:41 -0700

    integration webmvc with token

--- WebMVC/Infrastructure/CustomHttpClient.cs
+++ WebMVC/Infrastructure/CustomHttpClient.cs
@@ -19,9 +19,16 @@ namespace WebMVC.Infrastructure
             throw new NotImplementedException();
         }
 
-        public async Task<string> GetStringAsync(string uri, string authorizationtodekn = null, string authorizationmethod = "Bearer")
+        public async Task<string> GetStringAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer")
         {
             var requestMessage = new HttpRequestMessage(HttpMethod.Get,uri);
+
+            if (authorizationToken != null)
+            {
+                requestMessage.Headers.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue
+                    (authorizationMethod, authorizationToken);
+            }
             var response= await _client.SendAsync(requestMessage);
             return await response.Content.ReadAsStringAsync();
         }
