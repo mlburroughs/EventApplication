f17922e48aa9d788aac3725ea3c8b9e889f82ae4
gessyz123 <yasmine.a.elezaby@gmail.com>
2022-04-11 16:22:31 -0700

    catalog index view model

---
+++ WebMVC/ViewModels/EventCatalogIndexViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMVC.Models;
+
+namespace WebMVC.ViewModels
+{
+    public class EventCatalogIndexViewModel
+    {
+        public IEnumerable<SelectListItem> EventTypes { get; set; }
+        public IEnumerable<SelectListItem> EventCategories { get; set; }
+        public IEnumerable<SelectListItem> EventOrganizers { get; set; }
+        public IEnumerable<SelectListItem> EventMetroCities { get; set; }
+        public IEnumerable<EventItem> EventItems { get; set; }
+
+        public PaginationInfo PaginationInfo { get; set; }
+
+        public int? TypeFilterApplied { get; set; }
+        public int? CategoryFilterApplied { get; set; }
+        public int? OrganizerFilterApplied { get; set; }
+        public int? CityFilterApplied { get; set; }
+
+
+    }
+}
